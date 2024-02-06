e62a2267826428fc0bed06a560589176e5b99f2b
Marvinkwame <marvinkudjo@gmail.com>
2024-02-06 18:02:15 +0000

    Fixed Deployment errors

--- frontend/src/components/SeacrhBar.tsx
+++ frontend/src/components/SeacrhBar.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { useSearchContext } from '../context/SearchContext'
 import { CiLocationOn } from "react-icons/ci";
 import DatePicker from "react-datepicker";
